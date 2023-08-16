1c0bcc2f13f84b450c18e5904874d9bff96fcb9f
BLADE-HEDA <you@example.com>
2023-08-16 14:54:20 +0100

    log created

--- App.tsx
+++ App.tsx
@@ -1,24 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
 import { Provider } from 'react-redux';
 import store from "./store/store";
+import Login from './src/screens/Login';
 // import React from 'react'
 
 const App = () => {
   return (
-    <Provider store={store}> 
-    <View>
-      <Text style={styles.text} >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, totam.
-      </Text>
+    // <Provider store={store}> 
+    <View style={styles.container} >
+      <Login/>
     </View>
-    </Provider>
+    // </Provider>
   )
 }
 
 export default App
 
 const styles = StyleSheet.create({
-  text: {
-    color:'black',
+  container: {
+    backgroundColor:'white',
+    height:'100%',
+
   }
 })
---
+++ src/screens/Login.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Logo from '../component/shared/Logo'
+
+const Login = () => {
+  return (
+    <View>
+        <Logo/>  
+    </View>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({})
