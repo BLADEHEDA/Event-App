ea376f745e027a493aab358e1a90a4dc00bb3854
BLADE-HEDA <you@example.com>
2023-08-18 12:04:23 +0100

    changed to typescript

--- src/component/EventComponent.tsx
+++ src/component/EventComponent.tsx
@@ -1,27 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-const EventComponent = (props) => {
-  return (
-    <View style={styles.main}>
-        <Text style={styles.text}>{props.name}</Text>
-        <Text style={styles.text} >{props.email}</Text>
-    </View>
-  )
+interface EventComponentProps {
+  name: string;
+  email: string;
 }
 
-export default EventComponent
+const EventComponent: React.FC<EventComponentProps> = (props) => {
+  return (
+    <View style={styles.main}>
+      <Text style={styles.text}>{props.name}</Text>
+      <Text style={styles.text}>{props.email}</Text>
+    </View>
+  );
+};
+
+export default EventComponent;
 
 const styles = StyleSheet.create({
-    main:{
-        backgroundColor:'#1E319D',
-        paddingHorizontal:10,
-        paddingVertical:7,
-        borderRadius:8,
-        marginBottom:10,
-    },
-    text:{
-        color:'white',
-        fontSize:15
-    }
-})
+  main: {
+    backgroundColor: '#1E319D',
+    paddingHorizontal: 10,
+    paddingVertical: 7,
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+  text: {
+    color: 'white',
+    fontSize: 15,
+  },
+});
