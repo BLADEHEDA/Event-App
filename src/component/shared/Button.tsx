1ecc0d488c004d637e348acef68b821d076c404c
BLADE-HEDA <you@example.com>
2023-08-23 15:43:27 +0100

    changess done

--- src/component/shared/Button.tsx
+++ src/component/shared/Button.tsx
@@ -1,31 +1,32 @@
-import { StyleSheet, Text, View ,TouchableOpacity} from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React from 'react';
 
 const Button = (props) => {
   return (
     <View>
-       <TouchableOpacity  
-       onPress={props.onPress}
-       style={[styles.button,props.style]}>   
-      <Text style={styles.buttonText}>{props.text}</Text>
-    </TouchableOpacity>
+      <TouchableOpacity
+        onPress={props.onPress}
+        style={[styles.button, props.style]}
+      >
+        <Text style={styles.buttonText}>{props.text}</Text>
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
 
-export default Button
+export default Button;
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: '#1E319D',
-        paddingVertical: 15,
-        paddingHorizontal: 20,
-        borderRadius: 5,
-      },
-      buttonText:{
-        color:'white',
-        fontSize:18,
-        fontWeight:'500',
-        textAlign:'center'
-      }
-})
+  button: {
+    backgroundColor: '#1E319D',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+});
