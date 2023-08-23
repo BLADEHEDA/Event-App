88cf45aa5717b77f0f4a94cec8c30006379e9d0b
BLADE-HEDA <you@example.com>
2023-08-23 15:35:04 +0100

    changess done

--- store/store.ts
+++ store/store.ts
@@ -2,8 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
   reducer: {
-    // counter: counterReducer,
-    // Add more reducers here if needed
+    
   },
 });
 
